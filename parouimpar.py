fd7e784eb492d7887053b4f9346df68aaedafb23
Vrossi02 <96824480+Vrossi02@users.noreply.github.com>
2022-01-17 12:59:05 -0300

    Add files via upload
    Descubra se o numero é impar ou par.

---
+++ parouimpar.py
@@ -0,0 +1,7 @@
+#INSIRA UM NUMERO E DESCUBRA SE É PAR OU IMPAR
+
+n = int(input('Digite um número: '))
+if (n%2) == 0:
+    print(f'{n} é um número par.')
+else:
+    print(f'{n} é um número ímpar.')
